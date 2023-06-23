5775fa4f7e74a63767356f188dbc502c2364fe85
Crissdan93 <136115058+Crissdan93@users.noreply.github.com>
2023-06-23 16:01:32 -0600

    Add files via upload

---
+++ Nomina3.java
@@ -0,0 +1,13 @@
+package nomina;
+
+public class Nomina3 {
+	
+	
+	public double ClaculoHorasExtras(double horaextra, double horas ) {
+		
+		double horasextras = horaextra*horas;
+		return  horasextras;
+		
+	}
+
+}
